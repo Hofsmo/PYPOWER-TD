10119824793ded1600338ec5b753c08623cd63a7
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-08-24 10:17:23 +0200

    Trying to make travis not fail  Changes to be committed: 	new file:   PyPiW/__init__.py 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name=PyPiW,
+      version='0.1',
+      description='Testin setuptools, travis and pytest',
+      url='http://github.com/hofsmo/PyPiW',
+      author='Sigurd Hofsmo Jakobsen',
+      author_email='sigurd.jakobsenatgmail.com',
+      license='GPLv3',
+      packages=['PyPiW'],
+      zip_safe=False)
