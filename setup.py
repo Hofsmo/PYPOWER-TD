11572493409f53c9bb63194f7fab319d12c2d39e
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-08-24 13:00:49 +0200

    Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
 from setuptools import setup
 
-setup(name=PyPiW,
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='pypiw',
       version='0.1',
       description='Testin setuptools, travis and pytest',
       url='http://github.com/hofsmo/PyPiW',
       author='Sigurd Hofsmo Jakobsen',
       author_email='sigurd.jakobsenatgmail.com',
       license='GPLv3',
-      packages=['PyPiW'],
+      packages=['pypiw'],
       zip_safe=False)
