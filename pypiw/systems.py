bb1106d08e363d36368a086b7112122f78e35728
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-08-25 16:30:03 +0200

    On branch develop issue #1: Implemented a unit test verifying that a transfer function can be given as a sympy expression and that the time constants can be given as a list of intgers. The verification is done by the test comparing that a forced time repsonse gives the same reponse when given as sympy expression or list  Changes to be committed: 	modified:   pypiw/systems.py 	modified:   tests/test_systems.py

--- pypiw/systems.py
+++ pypiw/systems.py
@@ -1,5 +1,8 @@
 from abc import ABCMeta, abstractmethod
 import six
+import sympy
+import control
+import numpy as np
 
 
 @six.add_metaclass(ABCMeta)
@@ -23,5 +26,36 @@ class Tf(SystemBase):
     def __init__(self, sys):
         self.sys = sys
 
-    def time_response(self):
-        pass
+    def time_response(self, parameters, x, t):
+        """
+        Method that calculates the time response of the system
+        Input:
+            parameters: Value of the parameters in the system
+            x: Input vector
+            t: Time vector
+        Output:
+            numpy array containint the time response
+        """
+        # Define the laplace operator
+        s = sympy.symbols('s')
+
+        # Create lambda to generate transfer function
+        try:
+            f = sympy.lambdify(
+                self.sys.atoms(
+                    sympy.Symbol).difference({s}), self.sys, "numpy")
+        except TypeError:
+            print("Could not read system")
+
+        # Extract the numerator and denominator
+        temp = f(*parameters)
+
+        # The control toolbox does not understand sympy integers. Therefore
+        # it is necessary to convert
+        num = np.asarray(sympy.Poly(sympy.numer(temp)).all_coeffs(), float)
+        den = np.asarray(sympy.Poly(sympy.denom(temp)).all_coeffs(), float)
+
+        _, y, _ = control.forced_response(
+            control.tf(num, den), t,  x)
+
+        return y
--- tests/test_systems.py
+++ tests/test_systems.py
@@ -1,7 +1,27 @@
-
+import pytest
+import sympy
 from pypiw import systems
+import numpy as np
+import control
 
 
-def test_init():
-    a = systems.Tf(sys=1)
-    assert(a.sys == 1)
+@pytest.fixture(scope='session')
+def sys():
+    s, T1, T2 = sympy.symbols('s T1 T2')
+
+    return systems.Tf((1+s*T1)/(1+s*T2))
+
+
+def test_init(sys):
+    assert(isinstance(sys.sys, tuple(sympy.core.all_classes)))
+
+
+def test_time_response(sys):
+    t = np.arange(0, 0.5, 0.2)
+    x = np.ones(len(t))
+    tf = control.tf([2, 1], [-3, 1])
+    _, y, _ = control.forced_response(tf, t, x)
+
+    y_sys = sys.time_response([-3, 2], x, t)
+
+    np.testing.assert_array_equal(y, y_sys)
