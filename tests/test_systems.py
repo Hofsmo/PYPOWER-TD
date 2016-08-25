b8e26ec5243740259e8a2e388ab9ab8b952d6938
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-08-25 20:54:48 +0200

    On branch develop issue #1 Added hack to make tests run in both Python 2.7 and 3.5  Changes to be committed: 	modified:   tests/test_systems.py

--- tests/test_systems.py
+++ tests/test_systems.py
@@ -1,27 +1,33 @@
+import sys
 import pytest
 import sympy
+import control
 from pypiw import systems
 import numpy as np
-import control
 
 
 @pytest.fixture(scope='session')
-def sys():
+def tf():
     s, T1, T2 = sympy.symbols('s T1 T2')
 
     return systems.Tf((1+s*T1)/(1+s*T2))
 
 
-def test_init(sys):
-    assert(isinstance(sys.sys, tuple(sympy.core.all_classes)))
+def test_init(tf):
+    assert(isinstance(tf.sys, tuple(sympy.core.all_classes)))
 
 
-def test_time_response(sys):
+def test_time_response(tf):
     t = np.arange(0, 0.5, 0.2)
     x = np.ones(len(t))
-    tf = control.tf([2, 1], [-3, 1])
-    _, y, _ = control.forced_response(tf, t, x)
+    c_tf = control.tf([2.0, 1.0], [-3.0, 1.0])
+    _, y, _ = control.forced_response(c_tf, t, x)
 
-    y_sys = sys.time_response([-3, 2], x, t)
+    # The order of the parameters is different in some versions of Python.
+    # This does not matter for the identification
+    if sys.version_info >= (3, 5):
+        y_sys = tf.time_response([-3.0, 2.0], x, t)
+    else:
+        y_sys = tf.time_response([2.0, -3.0], x, t)
 
     np.testing.assert_allclose(y, y_sys)
