d1210faa7c26c6ff21b788c52aa35ca989ec1924
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2016-09-14 11:03:36 +0200

    On branch feature/tox #4  Changes to be committed: 	modified:   .gitignore 		- Ignore the tox folder 	modified:   .travis.yml 		- Install tox-travis and run tox 	modified:   examples/first_order.py 	modified:   pypiw/pypiw.py 		-Remove plotting stuff 	new file:   tox.ini 		-Simple tox configuration

--- examples/first_order.py
+++ examples/first_order.py
@@ -1,20 +1,23 @@
-from pypiw import algorithms, systems
 import numpy as np
 import control
 import sympy
-# Define the fitness function
+from pypiw import algorithms, systems
 
 
 def main():
+    """ Example demonstrating how to do a simple identification
+    """
+    # Create the dataset to work on
     t = np.arange(0, 20, 0.02)
     x = np.ones(len(t))
     tf = control.tf([2.0, 1.0], [-3.0, 1.0])
     _, y, _ = control.forced_response(tf, t, x)
-
+    
+    # Create the transfer function to identify using sympy
     s, T1, T2 = sympy.symbols('s T1 T2')
     sys = (1+s*T1)/(1+s*T2)
-
     tf = systems.Tf(sys)
+
     ga = algorithms.Ga(x, y, t, tf, -5.0, 5.0)
     ga.identify(verbose=True)
     print(ga.hof[0])
